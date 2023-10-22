db9c63109041271f8f2790f77bdb37488c6040eb
lana-z <lana@levelupeconomy.com>
2023-10-22 15:00:48 -0700

    added js file

---
+++ app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+alert("Hello! This website is about managing stress. Let's get started.")
+
+const userName = prompt("First, do you mind me asking your name?")
+
+const userStresslevel = prompt("Would you mind sharing: On a 1-10 scale, how stressed are you feeling today?")
+
+document.write("Ah, thanks for sharing that you're feeling stress at level " + userStresslevel + ".")
+
+if (userStresslevel <= 5){
+    document.write(" Glad you're feeling below average stress today!");
+}  
+    else {
+    document.write(" Rats, that's a lot of stress.")
+}
+
+const userLikelyhood = prompt("How likely are you to consider runnning as a stress reliever, " + userName + "?")
+
+if (userLikelyhood >= 5){
+    document.write(" Glad you're open to trying running to lower your stress, " + userName + ".");
+}  
+    else {
+    document.write(" Hopefully this site will help convince you to try running for stress relief, " + userName + ".")
+}
