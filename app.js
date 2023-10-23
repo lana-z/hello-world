88464bc898197acb72429342147b49a06cf041ae
lana-z <lana@levelupeconomy.com>
2023-10-22 22:19:09 -0700

    loop added but doesn't work

--- app.js
+++ app.js
@@ -3,9 +3,15 @@
 
 alert("Hello! This website is about managing stress. Let's get started.");
 
-const userName = prompt("First, do you mind me asking your name?")
+const userName = prompt("First, do you mind me asking your name?");
 
-const userStresslevel = prompt("Would you mind sharing: On a 1-10 scale, how stressed are you feeling today?")
+const userStresslevel = prompt("On a 1-10 scale, how stressed are you feeling today?")
+
+while (true) {
+    userStresslevel = prompt("On a 1-10 scale, how stressed are you feeling today?");
+    if (isNaN(userStresslevel) && userLikelyhood >= 1 && userStresslevel <= 10) 
+    }
+
 
 document.write("Ah, thanks for sharing that you're feeling stress at level " + userStresslevel + ".")
 
@@ -46,4 +52,12 @@ if (userLikelyhood >= 5){
     else {
     document.write(" Hopefully this site will help convince you to try running for stress relief, " + userName + ".")
 }
+function userStresslevel() {
 
+    let output = '';
+    let rating = prompt(userStresslevel);
+    for(let i = 0; i < rating; i++){
+        output += "<img src='https://images.unsplash.com/photo-1585089858717-f4378c2031d8?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fHN1bnNldHxlbnwwfHwwfHx8MA%3D%3D'/>";
+    }
+
+    return document.write(output);
